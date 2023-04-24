c512fd4a6535ebac51cd6f960e6ce591db782241
anhtuan112233 <125275942+anhtuan112233@users.noreply.github.com>
2023-04-24 20:06:58 +0700

    Add files via upload

---
+++ labth.java
@@ -0,0 +1,92 @@
+run:
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+1
+Nhap So Luong Muon Nhap Thong Tin: 
+1
+Khach Hang Thu : 1
+Nhap So Tai Khoan: 
+1111
+Nhap Ten Tai Khoan: 
+tuan
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+3
+Nhap So Tai Khoan Can Nap: 
+1111
+Nhap So Tien Can Nap: 2000
+Da Nap 2.000�? Vao Tai Khoan
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+4
+Nhap So Tai Khoan Can Rut: 
+1111
+Chon Tai Khoan: 1111
+Nhap So Tien Can Rut Ra: 1000
+Da Rut 1.000�?? Tu Tai Khoan 1111
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+5
+Nhap So Tai Khoan Chuyen Tien: 11110
+Nhap So Tai Khoan Nhan Tien: 1111
+
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+1
+Nhap So Luong Muon Nhap Thong Tin: 
+1
+Khach Hang Thu : 1
+Nhap So Tai Khoan: 
+2222
+Nhap Ten Tai Khoan: 
+vinh
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
+5
+Nhap So Tai Khoan Chuyen Tien: 1111
+Nhap So Tai Khoan Nhan Tien: 2222
+
+Vui Long Chon: 
+1.Nhap Thong Tin
+2.Xuat Danh Sach Thong Tin
+3.Nap Tien
+4.Rut Tien
+5.Chuyen Tien
+6.?�o H?n
+Nhan Phim 0 De Exit!
